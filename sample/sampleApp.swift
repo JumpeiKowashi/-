6200e69f3e7ba70ec9e7abdfd0c0cd37d359929f
Jumpei <k.juxxxxox@gmail.com>
2022-09-23 15:22:26 +0900

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Jumpei Kowashi on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
